464ed543c487e9601f53c044b5ea0a75d1a32ca6
Ananta Karmakar <codeantu@gmail.com>
2024-05-25 16:04:40 +0530

    Refactor authentication flow and update access token handling in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -29,38 +29,43 @@ const App: React.FC = () => {
       state: state,
     });
 
-  useEffect(() => {
-    if (accessToken && Date.now() - expired_at.getTime() < 0) {
-      navigate("/home");
-      // window.location.href = "/home";
-    }
+    console.log(client_id);
+    console.log(redirect_uri);
+    
+    
 
-    if (!accessToken && !code) {
-      console.log("no access token and no code");
-      window.location.href = Auth_url;
-    }
+  // useEffect(() => {
+  //   if (accessToken && Date.now() - expired_at.getTime() < 0) {
+  //     navigate("/home");
+  //     // window.location.href = "/home";
+  //   }
 
-    if (accessToken && Date.now() - expired_at.getTime() > 0) {
-      console.log("expired");
-      getRefreshToken(refreshToken).then((data) => {
-        console.log(data);
-        setAccessToken(data.access_token);
-        setExpired_at(new Date(new Date().getTime() + data.expires_in * 1000));
-        if (data.refresh_token) setRefreshToken(data.refresh_token);
-      });
-      // navigate("/home");
-    }
+  //   if (!accessToken && !code) {
+  //     console.log("no access token and no code");
+  //     window.location.href = Auth_url;
+  //   }
 
-    if (code) {
-      console.log("code");
-      getAccessToken(code).then((data) => {
-        setAccessToken(data.access_token);
-        setRefreshToken(data.refresh_token);
-        setExpired_at(new Date(new Date().getTime() + data.expires_in * 1000));
-      });
-      if (accessToken && expired_at.getTime() > Date.now()) navigate("/home");
-    }
-  });
+  //   if (accessToken && Date.now() - expired_at.getTime() > 0) {
+  //     console.log("expired");
+  //     getRefreshToken(refreshToken).then((data) => {
+  //       console.log(data);
+  //       setAccessToken(data.access_token);
+  //       setExpired_at(new Date(new Date().getTime() + data.expires_in * 1000));
+  //       if (data.refresh_token) setRefreshToken(data.refresh_token);
+  //     });
+  //     // navigate("/home");
+  //   }
+
+  //   if (code) {
+  //     console.log("code");
+  //     getAccessToken(code).then((data) => {
+  //       setAccessToken(data.access_token);
+  //       setRefreshToken(data.refresh_token);
+  //       setExpired_at(new Date(new Date().getTime() + data.expires_in * 1000));
+  //     });
+  //     if (accessToken && expired_at.getTime() > Date.now()) navigate("/home");
+  //   }
+  // });
 
   return (
     <div className="App">
