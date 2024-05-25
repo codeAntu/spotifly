231f25025b093038dd833e28142c45125e5084e2
Ananta Karmakar <codeantu@gmail.com>
2024-05-25 15:46:18 +0530

    w

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => {
   const setExpired_at = useStore((state) => state.setExpired_at);
   const navigate = useNavigate();
   const code = queryString.parse(window.location.search).code as string;
-  const client_id = "def12aa29212414cb49a5b71dc4f8131";
+  const client_id = import.meta.env.VITE_CLIENT_ID as string;
   const scope = "user-read-private user-read-email";
   const redirect_uri = import.meta.env.VITE_REDIRECT_URI as string;
   const state = "34fFs29kd09";
@@ -29,38 +29,46 @@ const App: React.FC = () => {
       state: state,
     });
 
-  useEffect(() => {
-    if (accessToken && Date.now() - expired_at.getTime() < 0) {
-      navigate("/home");
-      // window.location.href = "/home";
-    }
 
-    if (!accessToken && !code) {
-      console.log("no access token and no code");
-      window.location.href = Auth_url;
-    }
+    console.log(redirect_uri);
+    console.log(code);
+    console.log(accessToken);
+    console.log(Auth_url);
+    
+    
 
-    if (accessToken && Date.now() - expired_at.getTime() > 0) {
-      console.log("expired");
-      getRefreshToken(refreshToken).then((data) => {
-        console.log(data);
-        setAccessToken(data.access_token);
-        setExpired_at(new Date(new Date().getTime() + data.expires_in * 1000));
-        if (data.refresh_token) setRefreshToken(data.refresh_token);
-      });
-      // navigate("/home");
-    }
+  // useEffect(() => {
+  //   if (accessToken && Date.now() - expired_at.getTime() < 0) {
+  //     navigate("/home");
+  //     // window.location.href = "/home";
+  //   }
 
-    if (code) {
-      console.log("code");
-      getAccessToken(code).then((data) => {
-        setAccessToken(data.access_token);
-        setRefreshToken(data.refresh_token);
-        setExpired_at(new Date(new Date().getTime() + data.expires_in * 1000));
-      });
-      if (accessToken && expired_at.getTime() > Date.now()) navigate("/home");
-    }
-  });
+  //   if (!accessToken && !code) {
+  //     console.log("no access token and no code");
+  //     window.location.href = Auth_url;
+  //   }
+
+  //   if (accessToken && Date.now() - expired_at.getTime() > 0) {
+  //     console.log("expired");
+  //     getRefreshToken(refreshToken).then((data) => {
+  //       console.log(data);
+  //       setAccessToken(data.access_token);
+  //       setExpired_at(new Date(new Date().getTime() + data.expires_in * 1000));
+  //       if (data.refresh_token) setRefreshToken(data.refresh_token);
+  //     });
+  //     // navigate("/home");
+  //   }
+
+  //   if (code) {
+  //     console.log("code");
+  //     getAccessToken(code).then((data) => {
+  //       setAccessToken(data.access_token);
+  //       setRefreshToken(data.refresh_token);
+  //       setExpired_at(new Date(new Date().getTime() + data.expires_in * 1000));
+  //     });
+  //     if (accessToken && expired_at.getTime() > Date.now()) navigate("/home");
+  //   }
+  // });
 
   return (
     <div className="App">
